1e7ec635df185a24f8c18adb75d4706d99d0fd1f
Faron Bracy <signup@dreamersdesign.com>
2014-04-12 23:58:50 -0400

    + comments for UnionFind class

--- RogueSharp/Algorithms/UnionFind.cs
+++ RogueSharp/Algorithms/UnionFind.cs
@@ -3,19 +3,18 @@
 namespace RogueSharp.Algorithms
 {
    /// <summary>
-   /// 
+   /// The UnionFind class represents a union-find data type also known as the disjoint-sets data type.
+   /// It models connectivity among a set of N sites named 0 through N - 1.
    /// </summary>
-   /// <see>
-   ///    http://algs4.cs.princeton.edu/15uf/UF.java.html
-   ///    http://algs4.cs.princeton.edu/15uf
-   ///    Algorithms, 4th Edition by Robert Sedgewick and Kevin Wayne
-   /// </see>
+   /// <seealso cref="http://algs4.cs.princeton.edu/15uf/UF.java.html">
+   /// UF class from Princeton University's Java Algorithms
+   /// </seealso>
    public class UnionFind
    {
       private readonly int[] _id;
       private readonly int[] _size;
       /// <summary>
-      ///    Create an empty union-find data structure with "count" isolated sets
+      /// Create an empty union-find data structure with "count" isolated sets
       /// </summary>
       /// <param name="count">The number of isolated sets in the data strucutre</param>
       public UnionFind( int count )
@@ -34,14 +33,15 @@ namespace RogueSharp.Algorithms
          }
       }
       /// <summary>
-      /// 
+      /// Returns the number of components in this data structure
       /// </summary>
+      /// <returns>The number of components in this data structure</returns>
       public int Count { get; private set; }
       /// <summary>
-      ///    Return the id of the component corresponding to object p.
+      /// Returns the component identifier of the component containing site p
       /// </summary>
-      /// <param name="p"></param>
-      /// <returns></returns>
+      /// <param name="p">An integer representing one object</param>
+      /// <returns>The component identifier of the component containing site p</returns>
       public int Find( int p )
       {
          if ( p < 0 || p >= _id.Length )
@@ -55,20 +55,20 @@ namespace RogueSharp.Algorithms
          return p;
       }
       /// <summary>
-      ///    Are objects p and q in the same set?
+      /// Are objects p and q in the same set?
       /// </summary>
-      /// <param name="p"></param>
-      /// <param name="q"></param>
-      /// <returns></returns>
+      /// <param name="p">An integer representing one site</param>
+      /// <param name="q">An integer representing the other site</param>
+      /// <returns>true if the two sites p and q are in the same component; false otherwise</returns>
       public bool Connected( int p, int q )
       {
          return Find( p ) == Find( q );
       }
       /// <summary>
-      ///    Replace sets containing p and q with their union
+      /// Merges the component containing site p with the component containing site q
       /// </summary>
-      /// <param name="p"></param>
-      /// <param name="q"></param>
+      /// <param name="p">An integer representing one site</param>
+      /// <param name="q">An integer representing the other site</param>
       public void Union( int p, int q )
       {
          int i = Find( p );
