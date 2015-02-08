2a4a8060e9b856d3aba78a3f994443ff099eb252
Faron Bracy <signup@dreamersdesign.com>
2015-02-08 17:06:12 -0500

    + method for easy roll and sum of dice expression

---
+++ RogueSharp.Test/DiceNotation/DiceTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RogueSharp.DiceNotation;
+
+namespace RogueSharp.Test.DiceNotation
+{
+   [TestClass]
+   public class DiceTest
+   {
+      [TestMethod]
+      public void Roll_3D6_ResultBetween3And18()
+      {
+         int result = Dice.Roll( "3d6" );
+
+         Assert.IsTrue( result >= 3 && result <= 18 );
+      }
+
+      [TestMethod]
+      public void Roll_1D10Plus5_ResultBetween6And15()
+      {
+         int result = Dice.Roll( "1d10+5" );
+
+         Assert.IsTrue( result >= 6 && result <= 15 );
+      }
+
+      [TestMethod]
+      public void Roll_1D10Plus2d6Minus5_ResultBetweenNegative2And17()
+      {
+         int result = Dice.Roll( "1d10+2d6-5" );
+
+         Assert.IsTrue( result >= -2 && result <= 17 );
+      }
+   }
+}
--- RogueSharp/DiceNotation/Dice.cs
+++ RogueSharp/DiceNotation/Dice.cs
@@ -1,12 +1,23 @@
+using RogueSharp.Random;
+
 namespace RogueSharp.DiceNotation
 {
-    public static class Dice
-    {
-        private static readonly IDiceParser DiceParser = new DiceParser();
+   public static class Dice
+   {
+      private static readonly IDiceParser DiceParser = new DiceParser();
 
-        public static DiceExpression Parse(string expression)
-        {
-            return DiceParser.Parse(expression);
-        }
-    }
+      public static DiceExpression Parse( string expression )
+      {
+         return DiceParser.Parse( expression );
+      }
+
+      public static int Roll( string expression, IRandom random = null )
+      {
+         if ( random == null )
+         {
+            random = Singleton.DefaultRandom;
+         }
+         return Parse( expression ).Roll( random ).Value;
+      }
+   }
 }
