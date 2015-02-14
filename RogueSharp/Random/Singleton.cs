0ac8d3b4fba8304780973b4b00152c78ccc2d643
Faron Bracy <signup@dreamersdesign.com>
2015-02-14 12:47:01 -0500

    + comments for DefaultRandom

--- RogueSharp/Random/Singleton.cs
+++ RogueSharp/Random/Singleton.cs
@@ -1,7 +1,13 @@
 ﻿﻿namespace RogueSharp.Random
  {
+    /// <summary>
+    /// The Singleton class is a public static class that holds the DefaultRandom generator.
+    /// </summary>
     public static class Singleton
     {
+       /// <summary>
+       /// The DefaultRandom generator is DotNetRandom from System.Random
+       /// </summary>
        public static readonly DotNetRandom DefaultRandom = new DotNetRandom();
     }
  }
