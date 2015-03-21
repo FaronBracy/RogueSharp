35899465e500e27a5f13ef9293aed4558269148b
Glenn Hoeppner <glenn.hoeppner@yakhaircom>
2015-03-21 15:44:36 +0000

    Update copyright date

--- RogueSharp/Properties/AssemblyInfo.cs
+++ RogueSharp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "Dreamers Design" )]
 [assembly: AssemblyProduct( "RogueSharp" )]
-[assembly: AssemblyCopyright( "Copyright © Faron Bracy 2014" )]
+[assembly: AssemblyCopyright( "Copyright © Faron Bracy 2014-2015" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
