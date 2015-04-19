74e0d7b80f25f84bc6450bd421fd229f30e3c3c9
Glenn Hoeppner <glenn.hoeppner@yakhaircom>
2015-04-19 03:09:43 +0000

    Fix CA2104

--- RogueSharp/Random/Singleton.cs
+++ RogueSharp/Random/Singleton.cs
@@ -8,6 +8,7 @@
        /// <summary>
        /// The DefaultRandom generator is DotNetRandom from System.Random
        /// </summary>
+       /// CA2104 is thrown incorrectly on this line. Singleton.DefaultRandom is immutable.
        public static readonly DotNetRandom DefaultRandom = new DotNetRandom();
     }
  }
