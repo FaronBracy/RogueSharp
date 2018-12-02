7ac671873e0f08f881810f5fe49a33c74824000b
Faron Bracy <signup@dreamersdesign.com>
2018-12-01 21:33:52 -0500

    Creating tests for IndexMinPriorityQueue

---
+++ RogueSharp.Test/Algorithms/IndexMinPriorityQueueTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RogueSharp.Algorithms;
+
+namespace RogueSharp.Test.Algorithms
+{
+   [TestClass]
+   public class IndexMinPriorityQueueTest
+   {
+      [TestMethod]
+      public void IsEmpty_AfterConstructingNewQueue_WillBeTrue()
+      {
+         IndexMinPriorityQueue<double> queue = new IndexMinPriorityQueue<double>( 10 );
+
+         Assert.IsTrue( queue.IsEmpty() );
+      }
+
+      [TestMethod]
+      public void Size_AfterInserting1Key_WillBe1()
+      {
+         IndexMinPriorityQueue<double> queue = new IndexMinPriorityQueue<double>( 10 );
+         queue.Insert( 4, 12.5 );  
+
+         Assert.AreEqual( 1, queue.Size );
+      }
+   }
+}
