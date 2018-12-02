4850135645e126507183c2a14c21f462bb8dc773
Faron Bracy <signup@dreamersdesign.com>
2018-12-02 12:58:17 -0500

    + additional unit tests for IndexMinPriorityQueue

--- RogueSharp.Test/Algorithms/IndexMinPriorityQueueTest.cs
+++ RogueSharp.Test/Algorithms/IndexMinPriorityQueueTest.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RogueSharp.Algorithms;
 
 namespace RogueSharp.Test.Algorithms
@@ -21,9 +18,46 @@ namespace RogueSharp.Test.Algorithms
       public void Size_AfterInserting1Key_WillBe1()
       {
          IndexMinPriorityQueue<double> queue = new IndexMinPriorityQueue<double>( 10 );
-         queue.Insert( 4, 12.5 );  
+         queue.Insert( 4, 12.5 );
 
          Assert.AreEqual( 1, queue.Size );
+      }
+
+      [TestMethod]
+      public void Contains_Index4AfterInsertingKeyAtIndex4_WillBeTrue()
+      {
+         IndexMinPriorityQueue<double> queue = new IndexMinPriorityQueue<double>( 10 );
+         queue.Insert( 4, 12.5 );
+
+         Assert.IsTrue( queue.Contains( 4 ) );
+      }
+
+      [TestMethod]
+      public void MinIndex_WhenMultipleKeysAreInQueue_WillReturnIndexOfMinimumKey()
+      {
+         IndexMinPriorityQueue<double> queue = new IndexMinPriorityQueue<double>( 10 );
+         queue.Insert( 4, 12.5 );
+         queue.Insert( 3, 40.12 );
+         queue.Insert( 7, 4.3 );
+         queue.Insert( 2, 162.75 );
+
+         int minIndex = queue.MinIndex();
+         
+         Assert.AreEqual( 7, minIndex );
+      }
+
+      [TestMethod]
+      public void MinKey_WhenMultipleKeysAreInQueue_WillReturnMinimumKey()
+      {
+         IndexMinPriorityQueue<double> queue = new IndexMinPriorityQueue<double>( 10 );
+         queue.Insert( 4, 12.5 );
+         queue.Insert( 3, 40.12 );
+         queue.Insert( 7, 4.3 );
+         queue.Insert( 2, 162.75 );
+
+         double minKey = queue.MinKey();
+
+         Assert.AreEqual( 4.3, minKey );
       }
    }
 }
