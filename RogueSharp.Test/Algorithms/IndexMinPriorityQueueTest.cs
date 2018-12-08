eaaf68b45c697ec961b5a0a25bae9dd2da890813
Faron Bracy <signup@dreamersdesign.com>
2018-12-08 14:54:47 -0500

    + finish unit tests for IndexMinPriorityQueue

--- RogueSharp.Test/Algorithms/IndexMinPriorityQueueTest.cs
+++ RogueSharp.Test/Algorithms/IndexMinPriorityQueueTest.cs
@@ -42,7 +42,7 @@ namespace RogueSharp.Test.Algorithms
          queue.Insert( 2, 162.75 );
 
          int minIndex = queue.MinIndex();
-         
+
          Assert.AreEqual( 7, minIndex );
       }
 
@@ -58,6 +58,83 @@ namespace RogueSharp.Test.Algorithms
          double minKey = queue.MinKey();
 
          Assert.AreEqual( 4.3, minKey );
+      }
+
+      [TestMethod]
+      public void KeyAt_WhenIndexIsInQueue_WillReturnKeyAtIndex()
+      {
+         IndexMinPriorityQueue<double> queue = new IndexMinPriorityQueue<double>( 10 );
+         queue.Insert( 4, 12.5 );
+         queue.Insert( 3, 40.12 );
+         queue.Insert( 7, 4.3 );
+         queue.Insert( 2, 162.75 );
+
+         double keyAtIndex = queue.KeyAt( 3 );
+
+         Assert.AreEqual( 40.12, keyAtIndex );
+      }
+
+      [TestMethod]
+      public void ChangeKey_WhenIndexIsInQueue_WillChangeOldKeyToNewKey()
+      {
+         IndexMinPriorityQueue<double> queue = new IndexMinPriorityQueue<double>( 10 );
+         queue.Insert( 4, 12.5 );
+         queue.Insert( 3, 40.12 );
+         queue.Insert( 7, 4.3 );
+         queue.Insert( 2, 162.75 );
+         double newKey = 5.54;
+
+         queue.ChangeKey( 3, newKey );
+
+         double keyAtIndex = queue.KeyAt( 3 );
+         Assert.AreEqual( newKey, keyAtIndex );
+      }
+
+      [TestMethod]
+      public void DecreaseKey_WhenIndexIsInQueue_WillChangeOldKeyToNewSmallerKey()
+      {
+         IndexMinPriorityQueue<double> queue = new IndexMinPriorityQueue<double>( 10 );
+         queue.Insert( 4, 12.5 );
+         queue.Insert( 3, 40.12 );
+         queue.Insert( 7, 4.3 );
+         queue.Insert( 2, 162.75 );
+         double newKey = 5.54;
+
+         queue.DecreaseKey( 3, newKey );
+
+         double keyAtIndex = queue.KeyAt( 3 );
+         Assert.AreEqual( newKey, keyAtIndex );
+      }
+
+      [TestMethod]
+      public void IncreaseKey_WhenIndexIsInQueue_WillChangeOldKeyToNewBiggerKey()
+      {
+         IndexMinPriorityQueue<double> queue = new IndexMinPriorityQueue<double>( 10 );
+         queue.Insert( 4, 12.5 );
+         queue.Insert( 3, 40.12 );
+         queue.Insert( 7, 4.3 );
+         queue.Insert( 2, 162.75 );
+         double newKey = 5.54;
+
+         queue.IncreaseKey( 7, newKey );
+
+         double keyAtIndex = queue.KeyAt( 7 );
+         Assert.AreEqual( newKey, keyAtIndex );
+      }
+
+      [TestMethod]
+      public void Delete_WhenIndexIsInQueue_WillRemoveKeyFromQueue()
+      {
+         IndexMinPriorityQueue<double> queue = new IndexMinPriorityQueue<double>( 10 );
+         queue.Insert( 4, 12.5 );
+         queue.Insert( 3, 40.12 );
+         queue.Insert( 7, 4.3 );
+         queue.Insert( 2, 162.75 );
+
+         queue.Delete( 7 );
+
+         double keyAtIndex = queue.KeyAt( 7 );
+         Assert.AreEqual( default( double ), keyAtIndex );
       }
    }
 }
