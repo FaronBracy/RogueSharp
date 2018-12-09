361f3f1a54f88f901850e4f78dc56cffa2959f68
Faron Bracy <signup@dreamersdesign.com>
2018-12-09 11:42:07 -0500

    + unit test class for testing UnionFind

---
+++ RogueSharp.Test/Algorithms/UnionFindTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RogueSharp.Algorithms;
+
+namespace RogueSharp.Test.Algorithms
+{
+   [TestClass]
+   public class UnionFindTest
+   {
+      [TestMethod]
+      public void Count_WhenConstructorIsCalledWith5IsolatedSets_WillBe5()
+      {
+         UnionFind unionFind = new UnionFind( 5 );
+
+         int count = unionFind.Count;
+
+         Assert.AreEqual( 5, count );
+      }
+   }
+}
