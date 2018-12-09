766f60928f4f0cf549b5517df72c82d954661675
Faron Bracy <signup@dreamersdesign.com>
2018-12-09 08:59:28 -0500

    + unit test class for testing graph

---
+++ RogueSharp.Test/Algorithms/GraphTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RogueSharp.Algorithms;
+
+namespace RogueSharp.Test.Algorithms
+{
+   [TestClass]
+   public class GraphTest
+   {
+      [TestMethod]
+      public void NumberOfVertices_AfterConstructingNewGraphWith10Vertices_WillBe10()
+      {
+         Graph graph = new Graph( 10 );
+
+         int numberOfVertices = graph.NumberOfVertices;
+
+         Assert.AreEqual( 10, numberOfVertices );
+      }
+
+      [TestMethod]
+      public void NumberOfEdges_AfterConstructingNewGraphWith10Vertices_WillBe0()
+      {
+         Graph graph = new Graph( 10 );
+
+         int numberOfEdges = graph.NumberOfEdges;
+
+         Assert.AreEqual( 0, numberOfEdges );
+      }
+   }
+}
