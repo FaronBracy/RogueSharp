d835161170f3a653da06f10654114a452de49176
Faron Bracy <signup@dreamersdesign.com>
2018-12-23 15:49:17 -0500

    + unit tests for parameter checking in WeightedPool methods

--- RogueSharp.Test/Random/WeightedPoolTest.cs
+++ RogueSharp.Test/Random/WeightedPoolTest.cs
@@ -1,10 +1,56 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RogueSharp.Random;
 
 namespace RogueSharp.Test.Random
 {
    [TestClass]
    public class WeightedPoolTest
    {
+      [TestMethod]
+      public void Constructor_WhenIRandomArgumentIsNull_WillThrowArgumentNullException()
+      {
+         Assert.ThrowsException<ArgumentNullException>( () => new WeightedPool<int>( null ) );
+      }
 
+      [TestMethod]
+      public void Add_WhenItemArgumentIsNull_WillThrowArgumentNullException()
+      {
+         WeightedPool<string> pool = new WeightedPool<string>( Singleton.DefaultRandom );
+
+         Assert.ThrowsException<ArgumentNullException>( () => pool.Add( null, 1 ) );
+      }
+
+      [TestMethod]
+      public void Add_WhenWeightIs0_WillThrowArgumentException()
+      {
+         WeightedPool<int> pool = new WeightedPool<int>( Singleton.DefaultRandom );
+
+         Assert.ThrowsException<ArgumentException>( () => pool.Add( 12, 0 ) );
+      }
+
+      [TestMethod]
+      public void Add_WhenWeightIsNegative_WillThrowArgumentException()
+      {
+         WeightedPool<int> pool = new WeightedPool<int>( Singleton.DefaultRandom );
+
+         Assert.ThrowsException<ArgumentException>( () => pool.Add( 12, -5 ) );
+      }
+
+      [TestMethod]
+      public void Select_WhenPoolHas0Items_WillThrowInvalidOperationException()
+      {
+         WeightedPool<int> pool = new WeightedPool<int>( Singleton.DefaultRandom );
+
+         Assert.ThrowsException<InvalidOperationException>( () => pool.Select() );
+      }
+
+      [TestMethod]
+      public void Draw_WhenPoolHas0Items_WillThrowInvalidOperationException()
+      {
+         WeightedPool<int> pool = new WeightedPool<int>( Singleton.DefaultRandom );
+
+         Assert.ThrowsException<InvalidOperationException>( () => pool.Draw() );
+      }
    }
 }
--- RogueSharp/Random/WeightedPool.cs
+++ RogueSharp/Random/WeightedPool.cs
@@ -6,10 +6,30 @@ namespace RogueSharp.Random
    public class WeightedPool<T> : IWeightedPool<T>
    {
       private int _totalWeight;
+      private readonly IRandom _random;
       private readonly List<WeightedItem<T>> _pool = new List<WeightedItem<T>>();
+
+      public WeightedPool( IRandom random )
+      {
+         if ( random == null )
+         {
+            throw new ArgumentNullException( "Implementation of IRandom must not be null" );
+         }
+
+         _random = random;
+      }
 
       public void Add( T item, int weight )
       {
+         if ( item == null )
+         {
+            throw new ArgumentNullException( "Can not add null item to the pool" );
+         }
+         if ( weight <= 0 )
+         {
+            throw new ArgumentException( "Weight must be greater than 0" );
+         }
+
          WeightedItem<T> weightedItem = new WeightedItem<T>( item, weight );
          _pool.Add( weightedItem );
          _totalWeight += weight;
@@ -17,11 +37,19 @@ namespace RogueSharp.Random
 
       public T Select()
       {
+         if ( _pool.Count <= 0 || _totalWeight <= 0 )
+         {
+            throw new InvalidOperationException( "Add items to the pool before attempting to select one" );
+         }
          throw new NotImplementedException();
       }
 
       public T Draw()
       {
+         if ( _pool.Count <= 0 || _totalWeight <= 0 )
+         {
+            throw new InvalidOperationException( "Add items to the pool before attempting to draw one" );
+         }
          throw new NotImplementedException();
       }
 
