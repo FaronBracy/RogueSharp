220ee38ee88912fdf2753f73c2b3e99905e2b055
Faron Bracy <signup@dreamersdesign.com>
2018-12-23 10:37:50 -0500

    + unit tests for DepthFirstPaths

--- RogueSharp/Algorithms/DepthFirstPaths.cs
+++ RogueSharp/Algorithms/DepthFirstPaths.cs
@@ -60,7 +60,7 @@ namespace RogueSharp.Algorithms
       /// Returns a path between the sourceVertex and the specified destinationVertex or null if no such path exists
       /// </summary>
       /// <param name="destinationVertex">The destination vertex</param>
-      /// <returns>An IEnumerable sequence of vertices representing the path between the sourceVertex and the specified destinationVertex or null if no such path exists</returns>
+      /// <returns>An IEnumerable sequence of vertices representing the path between the sourceVertex and the specified destinationVertex or null if no such path exists. The path will not include the source vertex</returns>
       public IEnumerable<int> PathTo( int destinationVertex )
       {
          if ( !HasPathTo( destinationVertex ) )
@@ -68,6 +68,7 @@ namespace RogueSharp.Algorithms
             return null;
          }
          var path = new Stack<int>();
+         // TODO: All methods that return paths should be consistent on whether or not they include the source and destination vertices in the path.
          for ( int x = destinationVertex; x != _sourceVertex; x = _edgeTo[x] )
          {
             path.Push( x );
