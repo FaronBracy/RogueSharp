ee5ec2f86a1a2254014b760d08c0bd0d43e3b5ca
Faron Bracy <signup@dreamersdesign.com>
2019-02-10 13:33:42 -0500

    Initial placeholder for StringGenerator class

---
+++ RogueSharp.Test/Random/StringGeneratorTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RogueSharp.Random;
+
+namespace RogueSharp.Test.Random
+{
+   [TestClass]
+   public class StringGeneratorTest
+   {
+      [TestMethod]
+      public void AddWordPool_WhenWordPoolIsNull_ThrowsArgumentNullException()
+      {
+         StringGenerator stringGenerator = new StringGenerator();
+
+         Assert.ThrowsException<ArgumentNullException>( () => stringGenerator.AddWordPool( "color", null ) );
+      }
+   }
+}
---
+++ RogueSharp/Random/StringGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RogueSharp.Random
+{
+   public class StringGenerator
+   {
+      private Dictionary<string, IWeightedPool<string>> _wordPools;
+
+      public StringGenerator()
+      {
+         _wordPools = new Dictionary<string, IWeightedPool<string>>();
+      }
+
+      public void AddWordPool( string key, IWeightedPool<string> weightedWordPool )
+      {
+         if ( weightedWordPool == null )
+         {
+            throw new ArgumentNullException( nameof( weightedWordPool ), "Can not add null word pool" );
+         }
+
+         _wordPools.Add( key, weightedWordPool );
+      }
+   }
+}
